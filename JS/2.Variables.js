1f1a4f0bd3a42228c8a26d05e1b6e8c454443a19
Andrey Hernandez <hec_boy@hotmail.com>
2019-05-07 22:31:57 -0500

    Uso de variables

---
+++ JS/2.Variables.js
@@ -0,0 +1,7 @@
+var nombre, edad;
+
+nombre = prompt("Ingresa tu nombre", "nombre");
+edad = prompt("Ingresa tu edad");
+
+document.write("Mi nombre es "  + nombre + " y tengo " + edad + " años");
+//alert(nombre);
