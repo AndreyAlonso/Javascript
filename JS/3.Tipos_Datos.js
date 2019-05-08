7913f55753febd79f2e31ee02fe7a71835e1125c
Andrey Hernandez <hec_boy@hotmail.com>
2019-05-07 22:42:50 -0500

    Tipos de datos

---
+++ JS/3.Tipos_Datos.js
@@ -0,0 +1,26 @@
+//Numerico
+var edad = 21;
+
+//String - cadena texto
+var apPaterno = "Hernandez";
+var apMaterno = "\"Alonso\"";
+var nombre = "Héctor Andrey";
+
+var nCompleto = apPaterno + " " + apMaterno + " " + nombre;
+alert(nCompleto);
+
+//Array
+var Personas = ["Hugo", "Paco", "Luis"];
+document.write(Personas[0]);
+document.write(Personas[1]);
+document.write(Personas[2]);
+
+// Objetos
+
+var Objeto{
+	color: "Black",
+	size: 12
+}
+
+//Boolean 
+var band = false;
