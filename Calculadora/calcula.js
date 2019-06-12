8ade1b6c8412958f4b26646b017ed7899d1d2257
Andrey Hernandez <hec_boy@hotmail.com>
2019-06-11 22:48:44 -0500

    Calculadora en JavaScript (Basica)

--- Calculadora/calcula.js
+++ Calculadora/calcula.js
@@ -1,12 +1,28 @@
-var a, b;
-document.getElementById('formulario').addEventListener('submit',function(e){
-	a = document.getElementById('a').value;
-	b = document.getElementById('b').value;
-	e.preventDefault();
-});
 
-suma(){
-	alert(a);
-	document.getElementById('resp').value = a + b;
-	alert('suma');
+
+function suma(a, b){
+	var c = Number(a)+Number(b);
+	document.getElementById('resp').value = c;
+}
+
+function resta(a, b){
+	var c = Number(a)-Number(b);
+	document.getElementById('resp').value = c;
+}
+
+function division(a, b){
+	if(Number(b) == 0)
+	{
+		alert('No se puede dividir entre cero');
+	}
+	else{
+		var c = Number(a)/Number(b);
+	document.getElementById('resp').value = c;
+	}
+	
+}
+
+function producto(a, b){
+	var c = Number(a)*Number(b);
+	document.getElementById('resp').value = c;
 }
